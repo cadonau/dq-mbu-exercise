0bb47010ce62f0bf799f3bdb2b95c1774ff5eb9f
NRVR-M <94479077+NRVR-M@users.noreply.github.com>
2023-03-27 19:26:32 +0200

    First
    modifizieren/anpassen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test Index Site
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.20min.ch/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit 20Min
         </a>
       </header>
+
     </div>
   );
 }

fd11cdce9ff5c35d78599a1e577737fceef299f9
NRVR-M <94479077+NRVR-M@users.noreply.github.com>
2023-03-28 09:42:20 +0200

    removed unnecessary code

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,21 +8,7 @@ function About() {
                     About
                 </p>
             </header>
-            <body className="App-body">
-                <html>
 
-                    <head>
-                        <meta charset="utf-8" />
-                        <meta name="About" content="width=device-width" />
-                        <title>
-                            Home
-                        </title>
-                    </head>
-                    <body>
-                        <p>This is the About Screen</p>
-                    </body>
-                </html>
-            </body>
         </div>
 
     );

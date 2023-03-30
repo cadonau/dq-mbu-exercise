a19911dbd07ce813813dfd26c9928e9624145946
Markus Cadonau <mcadonau@dq-solutions.ch>
2023-03-30 18:05:41 +0200

    Add potential error message

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,6 +5,8 @@ export default function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const [errorMessage, setErrorMessage] = useState("");
+
     function handleUsernameChange(event) {
         setUsername(event.target.value);
     }
@@ -32,25 +34,35 @@ export default function Login() {
             .then(
                 data => {
                     console.log(data);
-                    // cf. https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
-                    localStorage.setItem("auth", data?.authToken);
+                    if (data && data.authToken) {
+                        // cf. https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+                        localStorage.setItem("auth", data?.authToken);
+                        setErrorMessage("");
+                    } else {
+                        setErrorMessage(data.message);
+                    }
                 }
             )
             .catch(error => console.error(error));
     }
 
     return (
-        // cf. https://getbootstrap.com/docs/5.2/forms/overview/
-        <form onSubmit={handleSubmit} className="container">
-            <label htmlFor="username" className="form-label">Username</label>
-            <input id="username" name="username" type="text" value={username} onChange={handleUsernameChange}
-                   required
-                   className="form-control"/>
-            <label htmlFor="password" className="form-label">Password</label>
-            <input id="password" name="password" type="password" value={password} onChange={handlePasswordChange}
-                   required
-                   className="form-control"/>
-            <input type="submit" className="btn btn-primary"/>
-        </form>
+        <>
+            {/*cf. https://getbootstrap.com/docs/5.2/forms/overview/*/}
+            <form onSubmit={handleSubmit} className="container">
+                <label htmlFor="username" className="form-label">Username</label>
+                <input id="username" name="username" type="text" value={username} onChange={handleUsernameChange}
+                       required
+                       className="form-control"/>
+                <label htmlFor="password" className="form-label">Password</label>
+                <input id="password" name="password" type="password" value={password} onChange={handlePasswordChange}
+                       required
+                       className="form-control"/>
+                <input type="submit" className="btn btn-primary"/>
+            </form>
+            <div className="container text-danger">
+                {errorMessage}
+            </div>
+        </>
     );
 }

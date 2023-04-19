192cc65c106a5946ed91b46b5d2cc0d7840c47cf
Markus Cadonau <mcadonau@dq-solutions.ch>
2023-04-19 09:57:32 +0200

    Split lunch registration form into multiple steps

--- src/pages/LunchRegistration.js
+++ src/pages/LunchRegistration.js
@@ -23,6 +23,9 @@ export default function LunchRegistration() {
         type: ""
     });
 
+    const TOTAL_STEPS = 3;
+    const [currentStep, setCurrentStep] = useState(0);
+
     useEffect(() => {
 
         // see returned cleanup function below
@@ -108,44 +111,61 @@ export default function LunchRegistration() {
 
     function resetForm() {
         setFormData(initialFormData);
+        setCurrentStep(0);
+    }
+
+    function handlePrev(event) {
+        if (0 < currentStep) {
+            setCurrentStep(prevState => prevState - 1);
+        }
     }
 
     async function handleSubmit(event) {
         event.preventDefault();
-        // cf. https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#uploading_json_data
-        try {
-            // cf. https://create-react-app.dev/docs/adding-custom-environment-variables/
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/dqdata`, {
-                method: "POST",
-                headers: {
-                    Authorization: `Bearer ${authToken}`,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    Key: crypto.randomUUID(),
-                    Text: JSON.stringify(formData)
-                })
-            });
-            if (!response.ok) {
-                setStatus({
-                    message: "Fehler beim Übermittlungs-Versuch: " + response.statusText,
-                    type: "error"
-                });
-                return;
+
+        // cf. https://developer.mozilla.org/en-US/docs/Web/API/Event/Comparison_of_Event_Targets
+        // cf. https://stackoverflow.com/questions/10086427/what-is-the-exact-difference-between-currenttarget-property-and-target-property
+        // cf. https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/reportValidity
+        if (event.currentTarget.reportValidity()) {
+            if (currentStep < TOTAL_STEPS - 1) {
+                setCurrentStep(prevState => prevState + 1);
+            } else {
+                // cf. https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#uploading_json_data
+                try {
+                    // cf. https://create-react-app.dev/docs/adding-custom-environment-variables/
+                    const response = await fetch(`${process.env.REACT_APP_API_URL}/dqdata`, {
+                        method: "POST",
+                        headers: {
+                            Authorization: `Bearer ${authToken}`,
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({
+                            Key: crypto.randomUUID(),
+                            Text: JSON.stringify(formData)
+                        })
+                    });
+                    if (!response.ok) {
+                        setStatus({
+                            message: "Fehler beim Übermittlungs-Versuch: " + response.statusText,
+                            type: "error"
+                        });
+                        return;
+                    }
+                    const responseObject = await response.json();
+                    console.log(responseObject);
+                    setStatus({
+                        message: "Erfolgreiche Anmeldung.",
+                        type: "success"
+                    });
+                    resetForm();
+                } catch (error) {
+                    console.error(error);
+                    setStatus({
+                        message: "Unerwarteter Fehler beim Übermittlungs-Versuch.",
+                        type: "error"
+                    });
+                }
             }
-            const responseObject = await response.json();
-            console.log(responseObject);
-            setStatus({
-                message: "Erfolgreiche Anmeldung.",
-                type: "success"
-            });
-            resetForm();
-        } catch (error) {
-            console.error(error);
-            setStatus({
-                message: "Unerwarteter Fehler beim Übermittlungs-Versuch.",
-                type: "error"
-            });
         }
     }
 
@@ -158,51 +178,66 @@ export default function LunchRegistration() {
 
             {/*cf. https://getbootstrap.com/docs/5.2/forms/overview/*/}
             <form onSubmit={handleSubmit} className="container">
-                <label htmlFor="person" className="form-label">Person:</label>
-                <select id="person" name="person" value={formData.person} onChange={handleChange}
-                        required
-                        className="form-select">
-                    <option value="">Person auswählen</option>
-                    {personOptions.map(({PersNr, NameVorname}) =>
-                        <option key={PersNr} value={PersNr}>{NameVorname}</option>
-                    )}
-                </select>
-                <label htmlFor="date" className="form-label">Datum:</label>
-                <input id="date" name="date" type="date" value={formData.date} onChange={handleChange}
-                       required
-                       className="form-control"/>
-                <label htmlFor="time" className="form-label">Zeit:</label>
-                <input id="time" name="time" type="time" value={formData.time} onChange={handleChange}
-                       min="12:00"
-                       max="13:00"
-                       step="3600"
-                       required
-                       className="form-control"/>
-                <fieldset>
-                    <legend>Angebot/Menü:</legend>
-                    <div className="form-check">
-                        <input id="vegetarian" name="lunchType" type="radio" value="vegetarian"
-                               checked={formData.lunchType === "vegetarian"} onChange={handleChange}
-                               className="form-check-input"
-                               required/>
-                        <label htmlFor="vegetarian" className="form-check-label">Vegetarisch</label>
-                    </div>
-                    <div className="form-check">
-                        <input id="meat" name="lunchType" type="radio" value="meat"
-                               checked={formData.lunchType === "meat"} onChange={handleChange}
-                               className="form-check-input"
-                               required/>
-                        <label htmlFor="meat" className="form-check-label">Fleisch</label>
-                    </div>
-                    <div className="form-check">
-                        <input id="salad-only" name="lunchType" type="radio" value="salad-only"
-                               checked={formData.lunchType === "salad-only"} onChange={handleChange}
-                               className="form-check-input"
-                               required/>
-                        <label htmlFor="salad-only" className="form-check-label">Nur Salat</label>
-                    </div>
-                </fieldset>
-                <input type="submit" className="btn btn-primary" value="Übermitteln"/>
+                {/* cf. https://react.dev/learn/conditional-rendering */}
+                {currentStep === 0 &&
+                    <fieldset>
+                        <label htmlFor="person" className="form-label">Person:</label>
+                        <select id="person" name="person" value={formData.person} onChange={handleChange}
+                                required
+                                className="form-select">
+                            <option value="">Person auswählen</option>
+                            {personOptions.map(({PersNr, NameVorname}) =>
+                                <option key={PersNr} value={PersNr}>{NameVorname}</option>
+                            )}
+                        </select>
+                    </fieldset>
+                }
+                {currentStep === 1 &&
+                    <fieldset>
+                        <label htmlFor="date" className="form-label">Datum:</label>
+                        <input id="date" name="date" type="date" value={formData.date} onChange={handleChange}
+                               required
+                               className="form-control"/>
+                        <label htmlFor="time" className="form-label">Zeit:</label>
+                        <input id="time" name="time" type="time" value={formData.time} onChange={handleChange}
+                               min="12:00"
+                               max="13:00"
+                               step="3600"
+                               required
+                               className="form-control"/>
+                    </fieldset>
+                }
+                {currentStep === 2 &&
+                    <fieldset>
+                        <legend>Angebot/Menü:</legend>
+                        <div className="form-check">
+                            <input id="vegetarian" name="lunchType" type="radio" value="vegetarian"
+                                   checked={formData.lunchType === "vegetarian"} onChange={handleChange}
+                                   className="form-check-input"
+                                   required/>
+                            <label htmlFor="vegetarian" className="form-check-label">Vegetarisch</label>
+                        </div>
+                        <div className="form-check">
+                            <input id="meat" name="lunchType" type="radio" value="meat"
+                                   checked={formData.lunchType === "meat"} onChange={handleChange}
+                                   className="form-check-input"
+                                   required/>
+                            <label htmlFor="meat" className="form-check-label">Fleisch</label>
+                        </div>
+                        <div className="form-check">
+                            <input id="salad-only" name="lunchType" type="radio" value="salad-only"
+                                   checked={formData.lunchType === "salad-only"} onChange={handleChange}
+                                   className="form-check-input"
+                                   required/>
+                            <label htmlFor="salad-only" className="form-check-label">Nur Salat</label>
+                        </div>
+                    </fieldset>
+                }
+                {0 < currentStep &&
+                    <input type="button" className="btn btn-outline-primary" value="Zurück" onClick={handlePrev}/>
+                }
+                <input type="submit" className="btn btn-primary"
+                       value={currentStep < TOTAL_STEPS - 1 ? "Weiter" : "Übermitteln"}/>
             </form>
             <div
                 className={`container ${status.type === "error" ? "text-danger" : null} ${status.type === "success" ? "text-success" : null}`}>
